3671f53618a683321d67dfcec777430d10154edd
ldsz89 <ldsz89@mail.missouri.edu>
2016-05-03 15:25:10 -0500

    Update stock.c

--- stock.c
+++ stock.c
@@ -23,6 +23,8 @@ void initializeProfits(int days, int transactions) {
     
     for(i = 0; i <= transactions; i++) {
         price[i] = 0;
+        buyDays[i] = 0;
+        sellDays[i] = 0;
         for(j = 0; j <= days; j++) {
             profit[i][j] = 0;
         }
@@ -30,20 +32,26 @@ void initializeProfits(int days, int transactions) {
 }
 
 void calculateProfits(int days, int transactions) {
-    int m = 1;
+    int m = 0;
     int currentDay = 1;
     int currentTrans = 1;
     
-    for(currentTrans = 1; currentTrans < transactions; currentTrans++) {
-        for(currentDay = 1; currentDay < days; currentDay++) {
+    for(currentTrans = 1; currentTrans <= transactions; currentTrans++) {
+        for(currentDay = 1; currentDay <= days; currentDay++) {
             int maxVal = 0;
             
             for(m = 0; m < currentDay; m++) {
+                int i = 0;
                 if((price[currentDay] - price[m] + profit[currentTrans - 1][m]) > maxVal) {
                     maxVal = (price[currentDay] - price[m] + profit[currentTrans - 1][m]);
                 }
-                if(profit[currentTrans][currentDay - 1] < maxVal)
+                if(profit[currentTrans][currentDay - 1] < maxVal && profit[currentTrans][currentDay] < maxVal) {
+                    //printf("Prev Day: %d\nToday: %d\n", profit[currentTrans][currentDay - 1], maxVal);
                     profit[currentTrans][currentDay] = maxVal;
+                    buyDays[i] = m;
+                    sellDays[i] = currentDay;
+                    //i++;
+                }
                 else
                     profit[currentTrans][currentDay] = profit[currentTrans][currentDay - 1];
             }
@@ -55,11 +63,15 @@ void printProfits(int days, int transactions) {
     int i;
     int j;
     
-    for(i = 1; i <= transactions; i++) {
+    for(i = 0; i <= transactions; i++) {
         printf("Transaction %d:\t", i);
-        for(j = 1; j <= days; j++) {
+        for(j = 0; j <= days; j++) {
             printf("%d\t", profit[i][j]);
         }
         printf("\n");
+        //printf("Buy: %d\tSell: %d\n\n", buyDays[i], sellDays[i]);
     }
+    
+    for(i = 0; i < transactions; i++)
+        printf("Buy: %d\tSell: %d\tProfit: %d\n\n", buyDays[i], sellDays[i], price[sellDays[i]] - price[buyDays[i]]);
 }
