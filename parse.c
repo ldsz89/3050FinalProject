aaa1d22ccb7804df253382b3db03845df04ee33a
ldsz89 <ldsz89@mail.missouri.edu>
2016-05-04 22:00:23 -0500

    Added files via upload

--- parse.c
+++ parse.c
@@ -12,16 +12,10 @@ void parse(FILE* fp, int size) {
         line[index] = c;
         count++;
         index++;
-        if(c == '\n' || c == '\r' || c == '\0'){    
-            temp = parse_test(line, count);
-            if(temp == 1) {
-                getDigits(line, size);
-                //printf("%d: RIght format\n", count2+1);
-            }
-            else{
-                //printf("%d: Wrong format\n", count2+1);
-                //exit(PARSING_ERROR_INVALID_FORMAT);
-            }
+        if(c == '\n' || c == '\r' || c == '\0') {
+            parse_test(line);
+            totalDays++;
+            getDigits(line, size);
             //printf("\n");
             index = 0;
             count = 0;
@@ -29,64 +23,35 @@ void parse(FILE* fp, int size) {
         }
         //index++;
     }
-    // The last input you wanna check
+    
     return;
 }
 
-int parse_test(char *input, int length) {
-    char* num1, num2;
-    //char right_format[3] = {'(', ',', ')'};
-    char temp[4] = {'\0', '\0', '\0', '\0'};
-    int index1 =0, index2 = 0, count = 0;
-    // printf("%d\n", length);
-    
-    while(index1  < length){
-        if(!(isdigit(input[index1]))){
-            //printf("%c", input[index1]);
-            temp[index2] = input[index1];
-            index2++;
+int parse_test(char *input) {
+    int i = 0;
+    char c = '\0';
+    c = input[i];
+        
+    while(c != '\0' || c != '\r', c != '\n') {
+        if(!isdigit(c)){
+            exit(PARSING_ERROR_INVALID_FORMAT);
         }
-        index1++;
+        i++;
+        c = input[i];
     }
     
-    index1 = 0;
-    char right_format[4] = {'(', ',',',', ')'};
-    count = 0;
-    while(index1 < 3){
-        // printf("%c", temp[index1]);
-        if(temp[index1] == right_format[index1]){
-            count++;
-        }
-        index1++;
-    }
-    
-    if(count == 3) return 1;
-    else return -1;
-   
+    return 1;
 }
 
 void getDigits(char* str, int size) {
     
     //printf("\nThe current line is: %s\n", str);
     
-    int v1, v2, w;
-    char *token = NULL;
-    char *token2 = NULL;
-    char *token3 = NULL;
-    token = strtok(str, ",()");
-    //printf("Token 1: %s\n", token);
-    token2 = strtok(NULL, ",)(");
-    //printf("Token 2: %s\n", token2);
-    token3 = strtok(NULL, ")(,");
-    //printf("Token 3: %s\n", token3);
+    int value = atoi(str);
     
-    v1 = atoi(token);
-    v2 = atoi(token2);
-    w = atoi(token3);
-    //printf("Vector: (%d,%d,%d)\n", v1, v2, w);
+    //printf("getDigits(%d)\n", value);
     
-    if(v1 > size || v1 < 1 || v2 > size || v2 < 1) exit(INTEGER_IS_NOT_A_VERTEX);
-    
-    //addEdge(graph, v1, v2, w);
-    addMatrixEdge(v1, v2, w);
+    if(value < 0) exit(INTEGER_IS_NOT_A_VERTEX);
+    addPrice(totalDays, value);
+    return;
 }
